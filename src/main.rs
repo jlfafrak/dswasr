02c232a7d1d403b28140728a4d34d5889947b5d2
James Fafrak <jim.fafrak@bluehalo.com>
2022-06-26 18:38:31 -0600

    Async stream on schedule w/ Tokio

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use chrono::prelude::*;
 use clap::Parser;
-use std::io::{Error, ErrorKind};
+use tokio::time;
+
+use std::{io::{Error, ErrorKind}, time::Duration};
 use yahoo_finance_api as yahoo;
 use async_trait::async_trait;
+use futures::future::join_all;
 
 #[derive(Parser, Debug)]
 #[clap(
@@ -15,6 +18,8 @@ struct Opts {
     symbols: String,
     #[clap(short, long)]
     from: String,
+    #[clap(short, long, default_value = "30")]
+    interval: u64
 }
 
 ///
@@ -153,49 +158,79 @@ async fn fetch_closing_data(
     }
 }
 
+async fn get_stock_info(
+    symbol: &str,
+    beginning: &DateTime<Utc>,
+    end: &DateTime<Utc>,
+) -> Option<Vec<f64>> {
+
+    let closes = fetch_closing_data(&symbol, &beginning, &end).await.ok()?;
+
+    if !closes.is_empty() {
+        let maxp = MaxPrice{};
+        let minp = MinPrice{};
+        let pdiff = PriceDifference{};
+        let wsma = WindowedSMA{ window_size: 30 };
+
+        // min/max of the period. unwrap() because those are Option types
+        let period_max: f64 = maxp.calculate(&closes).await.unwrap();
+        let period_min: f64 = minp.calculate(&closes).await.unwrap();
+        let last_price = *closes.last().unwrap_or(&0.0);
+
+        let (_, pct_change) = pdiff.calculate(&closes).await.unwrap_or((0.0, 0.0));
+        let sma = wsma.calculate(&closes).await.unwrap_or_default();
+
+        // a simple way to output CSV data
+        println!(
+            "{},{},${:.2},{:.2}%,${:.2},${:.2},${:.2}",
+            beginning.to_rfc3339(),
+            symbol,
+            last_price,
+            pct_change * 100.0,
+            period_min,
+            period_max,
+            sma.last().unwrap_or(&0.0)
+        );
+    }
+
+    Some(closes)
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let opts = Opts::parse();
     let from: DateTime<Utc> = opts.from.parse().expect("Couldn't parse 'from' date");
-    let to = Utc::now();
 
-    let maxp = MaxPrice{};
-    let minp = MinPrice{};
-    let pdiff = PriceDifference{};
-    let wsma = WindowedSMA{ window_size: 30 };
+
+    println!("Refresh interval: {} seconds.", opts.interval);
 
     // a simple way to output a CSV header
     println!("period start,symbol,price,change %,min,max,30d avg");
-    for symbol in opts.symbols.split(',') {
-        let closes = fetch_closing_data(&symbol, &from, &to).await?;
-        if !closes.is_empty() {
-                // min/max of the period. unwrap() because those are Option types
-                let period_max: f64 = maxp.calculate(&closes).await.unwrap();
-                let period_min: f64 = minp.calculate(&closes).await.unwrap();
-                let last_price = *closes.last().unwrap_or(&0.0);
-    
-                let (_, pct_change) = pdiff.calculate(&closes).await.unwrap_or((0.0, 0.0));
-                let sma = wsma.calculate(&closes).await.unwrap_or_default();
 
-            // a simple way to output CSV data
-            println!(
-                "{},{},${:.2},{:.2}%,${:.2},${:.2},${:.2}",
-                from.to_rfc3339(),
-                symbol,
-                last_price,
-                pct_change * 100.0,
-                period_min,
-                period_max,
-                sma.last().unwrap_or(&0.0)
-            );
-        }
+
+    // Create a timer to execute checks every 'opts.interval' seconds, by default this
+    // is 30 seconds, but can be modified via the --interval # command-line parameter.
+    let mut interval = time::interval(Duration::from_secs(opts.interval));
+    let symbols: Vec<&str> = opts.symbols.split(',').collect();
+
+
+    loop {
+        interval.tick().await;
+        
+        let to = Utc::now();
+        let requests: Vec<_> = symbols.iter()
+                                      .map(|&symbol| get_stock_info(&symbol, &from, &to))
+                                      .collect();
+        let _ = join_all(requests).await;
     }
-    Ok(())
+
 }
 
 #[cfg(test)]
 mod tests {
     #![allow(non_snake_case)]
+    use std::{fs::{self}};
+    
     use super::*;
 
     #[tokio::test]
@@ -262,4 +297,20 @@ mod tests {
         let signal = WindowedSMA { window_size: 10 };
         assert_eq!(signal.calculate(&series).await, Some(vec![]));
     }
+
+    #[tokio::test]
+    async fn test_GetSandP500Stocks_Succeeds() {
+        let stocks = fs::read_to_string("sp500.may.2020.txt").unwrap();
+        let symbols: Vec<&str> = stocks.split(',').collect();    
+        let from: DateTime<Utc> = "2020-05-01T12:00:09Z".parse().expect("Couldn't parse 'from' date");
+        let to = Utc::now();
+        let requests: Vec<_> = symbols.iter()
+                                      .map(|&symbol| get_stock_info(&symbol, &from, &to))
+                                      .collect();
+
+        let results = join_all(requests).await;
+
+        assert_eq!(505, results.len());
+    }
+
 }
